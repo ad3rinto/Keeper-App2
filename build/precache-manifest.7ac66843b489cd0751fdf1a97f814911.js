17fc8d21694f89d2a8e8270a825f826ea909abb9
Micheal A <aad3rinto@gmail.com>
2022-10-05 19:48:13 +0000

    run test

---
+++ build/precache-manifest.7ac66843b489cd0751fdf1a97f814911.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd76b56faefd9a58c3aa7112034fbf9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e0bfa52abe8f52825d2f",
+    "url": "/static/js/2.67111b69.chunk.js"
+  },
+  {
+    "revision": "e0da283c07c8c78ea2ad",
+    "url": "/static/js/main.8cfbd2b5.chunk.js"
+  },
+  {
+    "revision": "bc1bbb31d25428c430c4",
+    "url": "/static/js/runtime-main.fd9c2beb.js"
+  }
+]);
